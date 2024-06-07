4f7e0fb04ba41f37e730672e8f6df23f5d189599
joao fernando <jvieirafernando20@gmail.com>
2024-06-07 16:55:26 +0100

    remove secure in both email serve

--- services/email.service.js
+++ services/email.service.js
@@ -6,15 +6,10 @@ const transporter = nodemailer.createTransport({
   type: 'login',
   host: process.env.SMTP_HOST,
   port:process.env.SMTP_PORT,
-  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-
 });
 
 transporter.verify(function (error, success) {
